7700ec2c7a77338f12b854d34c33ae1af9ef1851
Vision-X <michael.s.marlow@gmail.com>
2018-09-25 15:38:18 -0600

    edit footer copy and contact info and social media links

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -2,25 +2,25 @@ import React from 'react';
 
 const ContactInfo = () => (
   <ul className="contact">
-    <li className="fa-home">
+    {/*<li className="fa-home">
       Untitled Inc
       <br />
       1234 Somewhere Road Suite #2894
       <br />
       Nashville, TN 00000-0000
-    </li>
-    <li className="fa-phone">(000) 000-0000</li>
+    </li>*/}
+    {/*<li className="fa-phone">(000) 000-0000</li>*/}
     <li className="fa-envelope">
-      <a href="#">information@untitled.tld</a>
+      <a href="mailto:michael.s.marlow@gmail.com">michael.s.marlow@gmail.com</a>
     </li>
     <li className="fa-twitter">
-      <a href="#">twitter.com/untitled-tld</a>
+      <a href="https://twitter.com/marlowcodes" target="_blank">twitter.com/marlowcodes</a>
     </li>
-    <li className="fa-facebook">
-      <a href="#">facebook.com/untitled-tld</a>
+    <li className="fa-github">
+      <a href="https://github.com/Vision-X" target="_blank">github.com/Vision-X</a>
     </li>
     <li className="fa-instagram">
-      <a href="#">instagram.com/untitled-tld</a>
+      <a href="https://instagram.com/mikemarlow" target="_blank">instagram.com/mikemarlow</a>
     </li>
   </ul>
 );
--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Copyright = ({ children }) => (
   <ul className="copyright">
-    <li>&copy; {new Date().getFullYear()} Untitled Inc. All rights reserved.</li>
+    <li>&copy; {new Date().getFullYear()} MarlowCodes. All rights reserved.</li>
     <li>
-      Design: <a href="http://html5up.net">HTML5 UP</a>
+      Design Help From: <a href="http://html5up.net">HTML5 UP</a>
     </li>
   </ul>
 );
